4a86a08e2698b359cbe0302193f3ece06fc92456
Ishaan <xonque@outlook.com>
2025-11-12 16:44:57 -0800

    Update universal.lua

--- Games/universal.lua
+++ Games/universal.lua
@@ -15,6 +15,7 @@ local virtualInput = cloneref(game:GetService('VirtualInputManager'))
 local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
 local coreGui = cloneref(game:GetService('CoreGui'))
 local userInputService = cloneref(game:GetService('UserInputService'))
+local runService = cloneref(game:GetService('RunService'))
 
 local keyMapping = {
     ['1'] = Enum.KeyCode.One,
@@ -37,6 +38,10 @@ local function hitKey(key, releaseDelay)
         virtualInput:SendKeyEvent(false,keyEnum,false,game)
     end
 end
+
+raldicrackhouse = {
+    Remotes = replicatedStorage.Remotes
+}
 
 for _, oldKavo in ipairs(coreGui:GetChildren()) do
     if oldKavo:IsA('ScreenGui') and oldKavo:FindFirstChild('Main') then
@@ -69,6 +74,8 @@ repeat
     kavo.Enabled = false
     task.wait()
 until not kavo.Enabled
+
+kavo.Name = 'Packet'
 
 local function toggleOverlay()
     kavo.Enabled = not kavo.Enabled
@@ -105,6 +112,8 @@ local function notify(titleText, descriptionText, duration)
     local header = mainFrame:FindFirstChild('MainHeader')
     if not header then return end
 
+    local uistroke = Instance.new('UIStroke')
+
     local notif = Instance.new("Frame")
     notif.BackgroundColor3 = header.BackgroundColor3
     notif.BorderSizePixel = 0
@@ -112,6 +121,8 @@ local function notify(titleText, descriptionText, duration)
     notif.AnchorPoint = Vector2.new(0.5,0.5)
     notif.Position = UDim2.new(0,0,0.85,0)
     notif.Parent = notificationframe
+
+    uistroke.Parent = notif
 
     local ucorner = Instance.new("UICorner")
     ucorner.CornerRadius = UDim.new(0,4)
@@ -144,15 +155,18 @@ local function notify(titleText, descriptionText, duration)
 
     local bar = Instance.new("Frame")
 
+    uistroke.Thickness = 1.5
+
     bar.Parent = notif
     local suc, res = pcall(function()
         bar.BackgroundColor3 = kavo.Main.MainSide.tabFrames.OverlayTabButton.BackgroundColor3
+        uistroke.Color = kavo.Main.MainSide.tabFrames.OverlayTabButton.BackgroundColor3
     end)
     if not suc then
+        uistroke.Color = Color3.fromRGB(255,255,255)
         bar.BackgroundColor3 = Color3.fromRGB(255,255,255)
     end
-    bar.BackgroundTransparency = 0.4
-    bar.Position = UDim2.new(0,0,0.924,0)
+    bar.Position = UDim2.new(0,0,0.943,0)
     bar.Size = UDim2.new(1,0,0,6)
 
     local barCorner = Instance.new("UICorner")
@@ -176,6 +190,10 @@ for _, kavoElement in ipairs(kavo:GetDescendants()) do
             kavoElement.Modal = true
         end
     end)
+end
+
+local function reloadGui()
+    loadstring(tostring(readfile(packetFolder..'/MainScript.lua')))()
 end
 
 if userInputService.TouchEnabled then
@@ -236,10 +254,23 @@ else
     kavo.Enabled = true
 end
 
-local crackTab = Window:NewTab("Universal")
+local uistroke = Instance.new('UIStroke')
+uistroke.Thickness = 1.5
+uistroke.Parent = kavo.Main
+
+task.spawn(function()
+    repeat
+        pcall(function()
+            uistroke.Color = kavo.Main.infoContainer:GetChildren()[3].BackgroundColor3
+        end)
+        task.wait()
+    until uistroke.Color == kavo.Main.infoContainer:GetChildren()[3].BackgroundColor3
+end)
+
+local universalTab = Window:NewTab("Universal")
 local overlayTab = Window:NewTab("Overlay")
 
-local crack = crackTab:NewSection("All universal exploits in one tab")
+local universal = universalTab:NewSection("All universal exploits in one tab")
 local overlay = overlayTab:NewSection("Change how the packet gui looks to your liking")
 
 overlay:NewDropdown("Theme", "Chooses your liking of theme.", {
@@ -270,10 +301,14 @@ local function isMobile()
 end
 
 if not isMobile() then
-    overlay:NewTextBox("Change Keybind", "Changes the overlay toggle keybind", function(val)
+    universal:NewTextBox("Change Keybind", "Changes the overlay toggle keybind", function(val)
         local key = tostring(string.upper(val:sub(0,1)))
 	    writefile(packetFolder..'/keybind.txt', key)
         keybind = key
         notify('Changed keybind! (Restart)', 'Toggle the overlay with: '..key)
     end)
 end
+
+universal:NewButton('Reload Packet', 'Restarts packet (make sure modules are off!)', function()
+    reloadGui()
+end)
